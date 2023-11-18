1378f2d0cadefe1c90e88689bb54546c2ff2504d
Victor Shurko <victorshurko@yandex.ru>
2023-11-18 17:55:34 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = b ** 2 - 4 * a * c;
+
+	if (discriminant > 0) {
+		let solutionFirst = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let solutionSecond = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(solutionFirst, solutionSecond);
+	} else if (discriminant === 0) {
+		let solution = -b / (2 * a);
+		arr.push(solution);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let percentPerMonths = (percent / 100) / 12;
+	let creditBody = amount - contribution;
+	let monthPay = creditBody * (percentPerMonths + (percentPerMonths / (((1 + percentPerMonths) ** countMonths) - 1)));
+	let total = Number((countMonths * monthPay).toFixed(2));
+
+	return total;
 }
